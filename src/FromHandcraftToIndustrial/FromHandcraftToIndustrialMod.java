df8d8ef72c0df20a925265c32a3ece02f44d7dd1
zsd_2024 <zsd_2024@outlook.com>
2024-10-02 17:02:59 +0800

    Update in VSCode

--- src/FromHandcraftToIndustrial/FromHandcraftToIndustrialMod.java
+++ src/FromHandcraftToIndustrial/FromHandcraftToIndustrialMod.java
@@ -2,13 +2,8 @@ package FromHandcraftToIndustrial;
 
 import FromHandcraftToIndustrial.content.block.ModBlocks;
 import FromHandcraftToIndustrial.content.item.ModItems;
-import arc.struct.Seq;
 import arc.util.Log;
-import mindustry.Vars;
 import mindustry.mod.Mod;
-import mindustry.world.Block;
-import mindustry.world.blocks.environment.Floor;
-import mindustry.world.meta.BuildVisibility;
 
 public class FromHandcraftToIndustrialMod extends Mod {
     public FromHandcraftToIndustrialMod() {
@@ -20,12 +15,5 @@ public class FromHandcraftToIndustrialMod extends Mod {
         Log.info("Loading some FromHandcraftToIndustrial content.");
         ModItems.load();
         ModBlocks.load();
-        Seq<Block> blocks = Vars.content.blocks();
-        for (Block block : blocks) {
-            if (block instanceof Floor) {
-                block.buildVisibility = BuildVisibility.shown;
-                block.alwaysUnlocked = true;
-            }
-        }
     }
 }
