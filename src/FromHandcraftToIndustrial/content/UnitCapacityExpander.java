2e07979a41b024ddb2dd77182e4bbd6103d062c3
zsd2024 <zsd_2024@outlook.com>
2024-10-07 15:22:10 +0800

    Remove mistake

--- src/FromHandcraftToIndustrial/content/UnitCapacityExpander.java
+++ src/FromHandcraftToIndustrial/content/UnitCapacityExpander.java
@@ -2,10 +2,6 @@ package FromHandcraftToIndustrial.content;
 
 import mindustry.gen.Building;
 import mindustry.world.Block;
-import mindustry.world.consumers.ConsumeItems;
-import mindustry.world.meta.Stat;
-import mindustry.world.meta.StatUnit;
-import mindustry.world.meta.StatValues;
 
 public class UnitCapacityExpander extends Block {
     public float reload = 60f;// 重新加载时间
@@ -22,20 +18,6 @@ public class UnitCapacityExpander extends Block {
     public UnitCapacityExpander(String name, int exp) {
         super(name);
         this.exp = exp;
-    }
-    @Override
-    public void setStats(){
-        stats.timePeriod = useTime;
-        super.setStats();
-
-        stats.add(Stat.speedIncrease, "+" + (int)(exp * 100f - 100) + "%");
-        stats.add(Stat.range, range / tilesize, StatUnit.blocks);
-        stats.add(Stat.productionTime, useTime / 60f, StatUnit.seconds);
-
-        if(hasBoost && findConsumer(f -> f instanceof ConsumeItems) instanceof ConsumeItems items){
-            stats.remove(Stat.booster);
-            stats.add(Stat.booster, StatValues.itemBoosters("+{0}%", stats.timePeriod, speedBoostPhase * 100f, phaseRangeBoost, items.items, this::consumesItem));
-        }
     }
 
     public class UnitCapacityExpanderBuild extends Building {
