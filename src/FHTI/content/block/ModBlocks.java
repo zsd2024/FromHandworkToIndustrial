3edfb62c16da5c21ef479fb40096db08ca3621a2
zsd2024 <zsd_2024@outlook.com>
2025-03-23 21:17:42 +0800

    Add some blocks

--- src/FHTI/content/block/ModBlocks.java
+++ src/FHTI/content/block/ModBlocks.java
@@ -8,14 +8,13 @@ import FHTI.content.block.wearable.wearableBlocks.defense.Wall;
 import FHTI.content.block.wearable.wearableBlocks.production.GenericCrafter;
 import FHTI.content.item.ModItems;
 import mindustry.content.Fx;
-import mindustry.content.Items;
 import mindustry.type.Category;
-import mindustry.type.ItemStack;
 import mindustry.world.meta.BuildVisibility;
 
 public class ModBlocks {
     public static Wall wood_wall;
     public static GenericCrafter log_cutter;
+    public static GenericCrafter plank_cutter;
 
     public static void load() {
         coreBlocks.load();
@@ -34,16 +33,30 @@ public class ModBlocks {
                 requirements(Category.crafting, with(ModItems.log, 10));
 
                 craftEffect = Fx.pulverizeMedium;
-                outputItem = new ItemStack(Items.graphite, 1);
-                craftTime = 90f;
+                outputItems = with(ModItems.wood_block, 6, ModItems.wood_chip, 30);
+                craftTime = 350f;
                 size = 4;
                 hasItems = true;
                 consumeItem(ModItems.log, 2);
+                serviceLife = 300;
+            }
+        };
+        plank_cutter = new GenericCrafter("plank-cutter") {
+            {
+                requirements(Category.crafting, with(ModItems.log, 5, ModItems.wood_block, 8));
+
+                craftEffect = Fx.pulverizeMedium;
+                outputItems = with(ModItems.wood_plank, 4, ModItems.wood_chip, 10);
+                craftTime = 200f;
+                size = 4;
+                hasItems = true;
+                consumeItem(ModItems.wood_block, 1);
+                serviceLife = 200;
             }
         };
         // Events.run(EventType.Trigger.update, () -> {
-        //     if (Vars.state.isPlaying() && Vars.state.isGame()) {
-        //     }
+        // if (Vars.state.isPlaying() && Vars.state.isGame()) {
+        // }
         // });
     }
 }
--- src/FHTI/content/item/ModItems.java
+++ src/FHTI/content/item/ModItems.java
@@ -6,6 +6,8 @@ import mindustry.type.Item;
 public class ModItems {
     public static Item log;
     public static Item wood_chip;
+    public static Item wood_block;
+    public static Item wood_plank; // 木板
 
     public static void load() {
         log = new Item("log", Color.valueOf("#8B4513")) {
@@ -17,11 +19,23 @@ public class ModItems {
                 alwaysUnlocked = true; // 始终解锁
             }
         };
-        wood_chip = new Item("wood-chip", Color.valueOf("#8B4513")) {
+        wood_chip = new Item("wood-chip", Color.valueOf("#A77756")) {
             {
                 flammability = 0.5f; // 易燃性
                 explosiveness = 0.3f; // 爆炸性
                 buildable = false; // 不可作为核心中的物品
+            }
+        };
+        wood_block = new Item("wood-block", Color.valueOf("#93572D")) {
+            {
+                flammability = 0.2f; // 易燃性
+                cost = 2; // 建造时间
+            }
+        };
+        wood_plank = new Item("wood-plank", Color.valueOf("#93572D")) {
+            {
+                flammability = 0.4f; // 易燃性
+                cost = 1; // 建造时间
             }
         };
     }
